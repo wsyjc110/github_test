8789b3507f56513f888145d3ca920d25ffec5d19
wsyjc110 <44385868+wsyjc110@users.noreply.github.com>
2019-05-21 16:28:00 +0800

    Update 图片增加数字.py

--- 图片增加数字.py
+++ 图片增加数字.py
@@ -1,11 +1,16 @@
 from PIL import Image,ImageDraw,ImageFont,ImageColor
 def add_num(im):
+    #创建一个draw对象
     draw = ImageDraw.Draw(im)
+    #设置字体和大小
     myfont = ImageFont.truetype(r'C:\Windows\Fonts\SIMYOU.TTF',size=50)
+    #设置颜色
     fillcolor = ImageColor.colormap.get('black')
+    #设置文本和在图片上哪里画
     draw.text((im.size[0]-200,0), '9', fill=fillcolor, font=myfont)
+    #保存
     im.save('test_af.jpg','jpeg')
-    return 0
+    return 
 if __name__ == '__main__':
     im = Image.open(r'F:\eclipse workspace\TestPython\MOOC\test_be.jpg')
     add_num(im)
